60e2a9499a27ca90f6424a0363e9a3dec07fe65d
Viktorysss <malikovavicktorias@gmail.com>
2025-07-16 17:30:35 +0300

    site

---
+++ footer.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.main-text-header__button--footer');
+
+button.addEventListener('click', () => {
+    result = prompt('Укажите ваше ФИ, и опишите заказ')
+});
+
+
+
+
+
+
